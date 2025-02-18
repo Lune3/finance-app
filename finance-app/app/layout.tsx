56fe29a217754b02dccefbc05050d034dfe8cd65
Lune3 <113820374+Lune3@users.noreply.github.com>
2025-02-18 17:51:31 +0000

    making api and react query for async task

--- finance-app/app/layout.tsx
+++ finance-app/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import {QueryProvider} from "@/providers/query-provider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,9 +27,10 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <QueryProvider>
+            {children}
+          </QueryProvider>
         </body>
       </html>
     </ClerkProvider>

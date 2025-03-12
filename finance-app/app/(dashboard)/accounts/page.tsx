0e648e82aa8201323b8123a14412ea3f3cf6ff8c
Lune3 <113820374+Lune3@users.noreply.github.com>
2025-03-12 20:40:56 +0000

    Bulk delete account api

--- finance-app/app/(dashboard)/accounts/page.tsx
+++ finance-app/app/(dashboard)/accounts/page.tsx
@@ -8,13 +8,17 @@ import {columns } from "./columns";
 import { DataTable } from "@/components/data-table";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete";
 
 
 
 const AccountsPage = () => {
     const newAccount = useNewAccount();
+    const deleteAccounts = useBulkDeleteAccounts();
     const accountsQuery = useGetAccounts();
     const accounts = accountsQuery.data || [];
+
+    const isDisable = accountsQuery.isLoading || deleteAccounts.isPending;
 
     if(accountsQuery.isLoading){
         return (
@@ -50,8 +54,11 @@ const AccountsPage = () => {
                     filterKey="email"
                     columns={columns}
                     data={accounts}
-                    onDelete={() => {}}
-                    disable={false}
+                    onDelete={(row) => {
+                        const ids = row.map((r) => r.original.id);
+                        deleteAccounts.mutate({ids})
+                    }}
+                    disable={isDisable}
                  />
                 </CardContent>
             </Card>

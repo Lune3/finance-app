8b89bfacaf9a044a3a09a553a82d355870fb81e7
Lune3 <113820374+Lune3@users.noreply.github.com>
2025-03-14 20:06:17 +0000

    account delete in moreHorizontal

--- finance-app/app/(dashboard)/accounts/actions.tsx
+++ finance-app/app/(dashboard)/accounts/actions.tsx
@@ -2,17 +2,29 @@
 
 import { Button } from "@/components/ui/button";
 import { DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
-import { Edit, MoreHorizontal } from "lucide-react";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
 import { useOpenAccount } from "@/features/accounts/hooks/use-open-accounts";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-accounts";
+import { useConfirm } from "@/hooks/use-confirm";
 
 type Props = {
     id: string;
 }
 
 export const Action = ({id} : Props) => {
+    const [ConfirmDialog,confirm] = useConfirm("Are you Sure?","You are about to delete this transaction.")
     const {onOpen} = useOpenAccount();
+    const deleteMutation = useDeleteAccount(id);
+    const handleDelete = async () => {
+        const ok = await confirm();
+
+        if(ok){
+            deleteMutation.mutate();
+        }
+    };
     return (
         <>
+            <ConfirmDialog/>
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <Button variant={"ghost"} className="size-8 p-0">
@@ -23,6 +35,10 @@ export const Action = ({id} : Props) => {
                     <DropdownMenuItem className="flex justify-center bg-slate-50 rounded-md px-3 py-1 cursor-pointer" disabled={false} onClick={() => onOpen(id)}>
                         <Edit className="size-4 mr-2"/>
                         Edit
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="flex justify-center bg-slate-50 rounded-md px-3 py-1 cursor-pointer" disabled={deleteMutation.isPending} onClick={handleDelete}>
+                        <Trash className="size-4 mr-2"/>
+                        Delete
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
--- finance-app/features/accounts/components/edit-account-sheet.tsx
+++ finance-app/features/accounts/components/edit-account-sheet.tsx
@@ -6,6 +6,8 @@ import { useOpenAccount } from "../hooks/use-open-accounts";
 import { useGetAccount } from "../api/use-get-account";
 import { Loader2 } from "lucide-react";
 import { useEditAccount } from "../api/use-edit-account";
+import { useDeleteAccount } from "../api/use-delete-accounts";
+import { useConfirm } from "@/hooks/use-confirm";
 
 const formSchema = insertAccountSchema.pick({
     name:true,
@@ -17,11 +19,14 @@ type FormValues = z.input<typeof formSchema>
 
 export const EditAccountSheet = () => {
     const {isOpen,onClose,id} = useOpenAccount();
+    const [ConfirmDialog,confirm] = useConfirm("Are you sure?","You are about to delete this transaction")
+
     const accountQuery = useGetAccount(id);
 
     const editMutation = useEditAccount(id);
+    const deleteMutation = useDeleteAccount(id);
 
-    const isPending = editMutation.isPending
+    const isPending = editMutation.isPending || deleteMutation.isPending;
 
     const isLoading = accountQuery.isLoading;
 
@@ -33,31 +38,46 @@ export const EditAccountSheet = () => {
         });
     }
 
+    const onDelete = async () => {
+        const ok = await confirm();
+
+        if(ok){
+            deleteMutation.mutate(undefined,{
+                onSuccess:() => {
+                    onClose();
+                }
+            })
+        }
+    }
+
     const defaultValues = accountQuery.data? {
         name: accountQuery.data.name
     }:{
         name:"",
     };
     return (
-        <Sheet open= {isOpen} onOpenChange={onClose}>
-            <SheetContent className="space-y-4">
-                <SheetHeader>
-                    <SheetTitle>
-                        Edit Account
-                    </SheetTitle>
-                    <SheetDescription>
-                        Edit an existing account
-                    </SheetDescription>
-                </SheetHeader>
-                {isLoading ?
-                 (
-                    <div className="absolute inset-0 flex items-center">
-                        <Loader2 className="size-4 text-muted-foreground animate-spin"/>
-                    </div>
-                 ) : (
-                     <AccountForm id={id} onSubmit={onSubmit} disable={isPending} defaultValues={defaultValues}/>
-                 )}
-            </SheetContent>
-        </Sheet>
+        <>
+            <ConfirmDialog/>
+            <Sheet open= {isOpen} onOpenChange={onClose}>
+                <SheetContent className="space-y-4">
+                    <SheetHeader>
+                        <SheetTitle>
+                            Edit Account
+                        </SheetTitle>
+                        <SheetDescription>
+                            Edit an existing account
+                        </SheetDescription>
+                    </SheetHeader>
+                    {isLoading ?
+                    (
+                        <div className="absolute inset-0 flex items-center">
+                            <Loader2 className="size-4 text-muted-foreground animate-spin"/>
+                        </div>
+                    ) : (
+                        <AccountForm id={id} onSubmit={onSubmit} disable={isPending} defaultValues={defaultValues} onDelete={onDelete}/>
+                    )}
+                </SheetContent>
+            </Sheet>
+        </>
     );
 };

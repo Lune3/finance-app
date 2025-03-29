a911965cebf965184614a938f06348b57fda0135
Lune3 <113820374+Lune3@users.noreply.github.com>
2025-03-29 20:22:41 +0000

    transaction form done

---
+++ finance-app/app/(dashboard)/transaction/actions.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
+import { useOpenAccount } from "@/features/accounts/hooks/use-open-accounts";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-accounts";
+import { useConfirm } from "@/hooks/use-confirm";
+
+type Props = {
+    id: string;
+}
+
+export const Action = ({id} : Props) => {
+    const [ConfirmDialog,confirm] = useConfirm("Are you Sure?","You are about to delete this account.")
+    const {onOpen} = useOpenAccount();
+    const deleteMutation = useDeleteAccount(id);
+    const handleDelete = async () => {
+        const ok = await confirm();
+
+        if(ok){
+            deleteMutation.mutate();
+        }
+    };
+    return (
+        <>
+            <ConfirmDialog/>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant={"ghost"} className="size-8 p-0">
+                        <MoreHorizontal className="size-4"></MoreHorizontal>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem className="flex justify-center bg-slate-50 rounded-md px-3 py-1 cursor-pointer" disabled={false} onClick={() => onOpen(id)}>
+                        <Edit className="size-4 mr-2"/>
+                        Edit
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="flex justify-center bg-slate-50 rounded-md px-3 py-1 cursor-pointer" disabled={deleteMutation.isPending} onClick={handleDelete}>
+                        <Trash className="size-4 mr-2"/>
+                        Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    )
+}
